d97f3b470459efd19690822c5c96c633dd2e2a72
finsignificant <74297078+finsignificant@users.noreply.github.com>
2020-12-20 14:42:19 +0000

    checkpoint

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,17 +1,8 @@
-let screenWidth = $(window).width();
-let percent30 = Math.floor((screenWidth / 10) * 3);
-let percent40 = Math.floor((screenWidth / 10) * 4);
-let percent50 = Math.floor(screenWidth / 2);
-let percent60 = Math.floor((screenWidth / 10) * 6);
-let percent70 = Math.floor((screenWidth / 10) * 7);
-let percent80 = Math.floor((screenWidth / 10) * 8);
-let percent90 = Math.floor((screenWidth / 10) * 9);
-
+const bookScreen = document.getElementById("book-screen");
 const bookText = document.querySelectorAll(".book-text-right, .book-text-left");
 const bookTextRight = document.querySelectorAll(".book-text-right");
 const bookTextLeft = document.querySelectorAll(".book-text-left");
 
-const bookScreen = document.getElementById("book-screen");
 const bookPageRight = document.getElementById("right-page");
 const bookPageLeft = document.getElementById("left-page");
 const bookBg = document.getElementById("book-bg");
@@ -37,7 +28,7 @@ bookText.forEach((line) => {
 bookTextRight.forEach((line) => {
   let spans = line.children;
   for (let i = 0; i < spans.length; i++) {
-    if (getPos(spans[i]).x > percent70) {
+    if (spans[i].offsetLeft > percents[5]) {
       spans[i].classList.add("blue");
     }
   }
@@ -46,34 +37,19 @@ bookTextRight.forEach((line) => {
 bookTextLeft.forEach((line) => {
   let spans = line.children;
   for (let i = 0; i < spans.length; i++) {
-    if (getPos(spans[i]).x < percent30) {
+    if (spans[i].offsetLeft < percents[5]) {
       spans[i].classList.add("red");
+      // console.log(spans[0].offsetLeft + spans[0].innerText);
     }
   }
 });
 
 window.onclick = () => {
-  // console.log("remove");
   bookText.forEach((line) => {
     let spans = line.children;
-    // console.log(spans);
     for (let i = 0; i < spans.length; i++) {
       span = spans[i];
-      // console.log(span);
       span.removeAttribute("class");
     }
   });
 };
-// window.onscroll = addCurve();
-// // find position of each span
-function getPos(el) {
-  // needs to be relative to #book screen
-  // yay readability
-  for (
-    var lx = 0, ly = 0;
-    el != null;
-    lx += el.offsetLeft, ly += el.offsetTop, el = el.offsetParent
-  );
-  return { x: lx, y: ly };
-}
-// // https://stackoverflow.com/questions/288699/get-the-position-of-a-div-span-tag
