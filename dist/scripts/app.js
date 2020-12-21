86e5570819d4c188ec5b65acdb72b71d3f592afb
finsignificant <74297078+finsignificant@users.noreply.github.com>
2020-12-21 14:55:09 +0000

    add js clas generation

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -27,21 +27,34 @@ bookText.forEach((line) => {
 });
 
 let percent = [];
-const num = 40;
+const num = 50;
 for (let i = 0; i < num; i++) {
   percent.push((bookPageLeft.offsetWidth / num) * [i]);
+  const element = document.createElement("style");
+  let shiftLeft = Math.cos(i * 0.1) + "rem";
+  // element.id = "myStyleId"; // so you can get and alter/replace/remove later
+  element.innerHTML =
+    ".left-curve" + i + "{transform:translateY(" + shiftLeft + ");}";
+  var header = document.getElementsByTagName("HEAD")[0];
+  header.appendChild(element);
+  const elementRight = document.createElement("style");
+  let shiftRight = Math.cos(i * 0.1 - 180) + "rem";
+  // element.id = "myStyleId"; // so you can get and alter/replace/remove later
+  elementRight.innerHTML =
+    ".right-curve" + i + "{transform:translateY(" + shiftRight + ");}";
+  var header = document.getElementsByTagName("HEAD")[0];
+  header.appendChild(elementRight);
 }
-
 bookTextRight.forEach((line) => {
   let spans = line.children;
   for (let i = 0; i < spans.length; i++) {
     for (let j = 0; j < percent.length; j++) {
       if (
-        spans[i].offsetLeft >= percent[j] &&
-        spans[i].offsetLeft <= percent[j + 1]
+        spans[i].offsetLeft + spans[i].offsetWidth / 2 >= percent[j] &&
+        spans[i].offsetLeft + spans[i].offsetWidth / 2 <= percent[j + 1]
       ) {
+        // let rightAdjust = 50 - j;
         spans[i].classList.add("right-curve" + j);
-        spans[i].classList.add("letters");
       }
     }
   }
@@ -52,11 +65,10 @@ bookTextLeft.forEach((line) => {
   for (let i = 0; i < spans.length; i++) {
     for (let j = 0; j < percent.length; j++) {
       if (
-        spans[i].offsetLeft + spans[i].offsetWidth >= percent[j] &&
-        spans[i].offsetLeft + spans[i].offsetWidth <= percent[j + 1]
+        spans[i].offsetLeft + spans[i].offsetWidth / 2 >= percent[j] &&
+        spans[i].offsetLeft + spans[i].offsetWidth / 2 <= percent[j + 1]
       ) {
         spans[i].classList.add("left-curve" + j);
-        spans[i].classList.add("letters");
       }
     }
   }
