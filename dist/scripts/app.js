1968eb004bf57c167dca1ccf9b097039c7ef51f8
finsignificant <74297078+finsignificant@users.noreply.github.com>
2021-01-03 11:17:25 +0000

    debeloper animations

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -359,31 +359,28 @@ softSkills.addEventListener("mousemove", (e) => {
 });
 const developerCards = document.querySelectorAll(".developer-card");
 const developerBgs = document.querySelectorAll(".developer-bg");
-console.log(developerBgs);
-const shiftDiv = 5;
+
 developerCards.forEach((card) => {
-  let rect = card.getBoundingClientRect();
-  let x = rect.width / 2; //x position within the element.
-  let y = rect.height / 2; //y position within the element.
   card.addEventListener("mouseover", (e) => {
-    let rect = card.getBoundingClientRect();
-    let x = e.clientX - rect.left; //x position within the element.
-    let y = e.clientY - rect.top; //y position within the element.
-    console.log(x, y);
-
-    card.children[0].style.transform = "scale(1)";
+    card.children[0].classList.add("normalize");
+    // card.children[1].classList.add("normalize");
     card.children[1].style.transform = "scale(1)";
-    card.children[1].style.transform +=
-      "translate(" + x / shiftDiv + "px," + y / shiftDiv + "px)";
-
     card.children[2].style.transform = "scale(1)";
-    card.children[2].style.transform +=
-      "translate(" + x / shiftDiv + "px," + y / shiftDiv + "px)";
   });
-  // card.addEventListener("mouseout", (e) => {
-  //   window.clearInterval();
-  //   developerBgs.forEach((bg) => {
-  //     bg.style.transform = "scale(0)";
-  //   });
-  // });
+  card.addEventListener("mouseout", (e) => {
+    card.children[0].classList.remove("normalize");
+
+    card.children[1].style.transform = "scale(0)";
+    card.children[2].style.transform = "scale(0)";
+  });
+  card.addEventListener("mousemove", (e) => {
+    let rect = card.children[0].getBoundingClientRect();
+    let mouseX = e.clientX - rect.left - 250;
+    let mouseY = e.clientY - rect.top - 250;
+    card.children[1].style.transform =
+      "translate(" + mouseX / 2 + "px," + mouseY / 2 + "px)";
+    console.log(mouseX, mouseY);
+    card.children[2].style.transform =
+      "translate(" + mouseX + "px," + mouseY + "px)";
+  });
 });
